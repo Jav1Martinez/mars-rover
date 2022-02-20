4a0ae2bb8cdfa05942d478ba2f2436fc4ce196e5
Javier Martinez <javier.martinezabarca@codurance.com>
2022-02-20 04:26:36 +0100

    Second step - Right rotation.
    Added rotation test to cover three times RIGHT spin ("RRR" command) rotation from South to West.

--- Tests/MarsRoverShould.cs
+++ Tests/MarsRoverShould.cs
@@ -43,5 +43,18 @@ namespace Tests
             // Assert
             Assert.Equal("0:0:S", result);
         }
+        
+        [Fact]
+        public void RotateRightFromSouth_ThenWestIsFacing()
+        {
+            // Arrange
+            var command = "RRR";
+            
+            // Act
+            var result = new MarsRover.MarsRover().Execute(command);
+            
+            // Assert
+            Assert.Equal("0:0:W", result);
+        }
     }
 }
